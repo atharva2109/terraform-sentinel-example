a9970032d7cd7ba1a4990c34a70d23026d619cfb
Atharva Kulkarni <64882461+atharva2109@users.noreply.github.com>
2023-02-10 10:25:40 +0530

    Added variables.tf file

--- main.tf
+++ main.tf
@@ -8,15 +8,13 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIATWAZVG7Z2XIZGEWP"
-  secret_key = "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"
+  region = "us-east-1"
 }
 
 module "apache" {
   source        = "atharva2109/apache/aws"
   version       = "1.1.0"
-  instance_type = "t2.micro"
-  ip_address    = "165.225.120.220/32"
-  vpc_id        = "vpc-058194cc3a8f7761d"
+  instance_type = var.instance_type
+  ip_address    = var.ip_address
+  vpc_id        = var.vpc_id
 }
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ip_address" {
+  type        = string
+  description = "Provide your IP Address in the form of 165.225.120.220/32"
+}
+
+variable "vpc_id" {
+  type = string
+}
